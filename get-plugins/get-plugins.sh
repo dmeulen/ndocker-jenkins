f6c24c6d27508123ebb5d0528127388ad5c7fa3d
Danny van der Meulen <dmeulen@ebay.com>
2019-05-27 00:12:24 +0200

    Install latest plugins

--- get-plugins/get-plugins.sh
+++ get-plugins/get-plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-JENKINS_HOST="http://192.168.1.253:8080"
+JENKINS_HOST="http://ci.anna.lan"
 USERNAME=$(zenity --entry --text "Username")
 PASSWORD=$(zenity --password --text "Password")
 
-java -jar ./jenkins-cli.jar -s ${JENKINS_HOST} groovy --username ${USERNAME} --password ${PASSWORD} = < ./plugins.groovy | sed -e 's/: /:/' > plugins.txt
+java -jar ./jenkins-cli.jar -s ${JENKINS_HOST} -auth ${USERNAME}:${PASSWORD} groovy = < ./latest-plugins.groovy | sed -e 's/: /:/' > plugins.txt
